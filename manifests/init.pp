8c3506cb2c7a14a05b530ca1474bcaf766e5f3bf
Anton Samets <a.samets@gmail.com>
2024-07-01 12:22:52 +0300

    remove outdated validate, add Enum to udev log
    Remove legacy validate functions, set correct Enum for udev log

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,12 +20,10 @@
 # include udev
 #
 class udev(
-  String           $udev_log            = $udev::params::udev_log,
-  Boolean          $config_file_replace = $udev::params::config_file_replace,
-  Optional[String] $rules               = $udev::params::rules,
+  Enum['err', 'info', 'debug']  $udev_log            = $udev::params::udev_log,
+  Boolean                       $config_file_replace = $udev::params::config_file_replace,
+  Optional[String]              $rules               = $udev::params::rules,
 ) inherits udev::params {
-  validate_re($udev_log, '^err$|^info$|^debug$')
-  validate_bool($config_file_replace)
 
   anchor { 'udev:begin': }
   -> package{ $udev::params::udev_package:
